22738f668d27fd6c7933f3f143696b99e29a210d
Dan Stutzman <dan@danstutzman.com>
2017-01-09 20:00:29 -0700

    Run go install before go vet for quicker compiler errors

--- go.sh
+++ go.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -ex
-pushd $GOPATH/src/github.com/danielstutzman/prometheus-email-reports
-go vet .
-pushd
-
 go install .
+
 rm -f out.png
 $GOPATH/bin/prometheus-email-reports -pngPath out.png -prometheusHostPort localhost:9090
 open out.png
